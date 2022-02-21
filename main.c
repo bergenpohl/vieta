1cc45b646714b48af6774d22e99bf6401fbbd003
Bergen Pohl <circidalia@gmail.com>
2022-02-21 18:22:35 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int	main() {
+	double	pi;
+	double	res;
+	double	last;
+
+	res = 1;
+	last = sqrt(2);
+	for (unsigned int i = 0; i < 30; i++) {
+		res *= (last / 2);
+		last = sqrt(2 + last);
+	}
+	pi = 2 / res;
+	printf("pi g : %.30f\n", pi);
+	return (0);
+}
+
